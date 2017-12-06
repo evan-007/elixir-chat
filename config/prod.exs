91ec4df1042814d7168b27c8a993cf2eb05a7c2a
Evan Lloyd <elloyd@patientslikeme.com>
2017-12-06 07:55:32 -0500

    Add config for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -15,12 +15,21 @@ use Mix.Config
 # which you typically run after static files are built.
 config :plm_live, PLMLiveWeb.Endpoint,
   load_from_system_env: true,
-  check_origin: ["https://wow-chat.stage.patientslikeme.com"]
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  check_origin: ["https://wow-chat.stage.patientslikeme.com"],
+  url: [scheme: "https", host: "thawing-brook-24305.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure your database
+config :plm_live, PLMLive.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 15,
+  ssl: true
 
 # ## SSL Support
 #
@@ -62,4 +71,3 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
